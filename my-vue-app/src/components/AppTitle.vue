d162f91c5d638f9f192689ca4c8270393dbf4e28
paolocastaldi <pao.gamer97@gmail.com>
2023-02-28 17:21:51 +0100

    first commit

---
+++ my-vue-app/src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="bg-red">
+    {{ descrizione }}
+
+</div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            descrizione: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<style>
+.bg-red {
+    background-color: red;
+    color: white;
+}
+</style>
